1cce69752a625229ee6f3e0e220a99f1166cf985
CmlMagneville <camille.magneville@gmail.com>
2024-04-18 13:32:24 +0200

    :green_heart: Try rf for trees (bad) and birds (ok)

---
+++ analysis/4_a_Impute_traits_TREES.R
@@ -0,0 +1,96 @@
+################################################################################
+##
+## Script to impute traits based on missForests - for TREES
+##
+## Camille Magneville
+##
+## 12/04/2024
+##
+## 4_a_Impute_traits_TREES.R
+##
+################################################################################
+
+
+# Define the pipe symbol so I can use it:
+`%>%` <- magrittr::`%>%`
+
+
+# 1 - Load data ========================================================
+
+
+sp_tr_TREES <- readRDS(here::here("transformed_data",
+                                  "raw_traits_TREES.rds"))
+
+
+# 2 - Traits and table in the right format ==============================
+
+
+sp_tr_TREES$LA <- as.numeric(sp_tr_TREES$LA)
+sp_tr_TREES$SeedMass <- as.numeric(sp_tr_TREES$SeedMass)
+sp_tr_TREES$SLA <- as.numeric(sp_tr_TREES$SLA)
+sp_tr_TREES$StemSpecDens <- as.numeric(sp_tr_TREES$StemSpecDens)
+sp_tr_TREES$HeightMax <- as.numeric(sp_tr_TREES$HeightMax)
+
+# Species as rownames:
+sp_tr_TREES <- tibble::column_to_rownames(sp_tr_TREES,
+                                          "Species")
+
+
+# 3 - Impute traits based on Random Forest approach =====================
+
+
+set.seed(42)
+
+# Impute traits and check NRMSE - with missForest R pkge:
+imputed_traits_TREES_test <- missForest::missForest(sp_tr_TREES,
+                                                    maxiter = 100,
+                                                    ntree = 200)
+try <- imputed_traits_TREES_test$ximp
+error <- imputed_traits_TREES_test$OOBerror
+
+
+# Impute traits and check quality - with mice pkge:
+## Check missing traits:
+mice::md.pattern(sp_tr_TREES)
+## Compute missing data:
+init_test <- mice::mice(sp_tr_TREES, ntree = 100,
+                        m = 6,
+                        meth = 'rf', seed = 42)
+summary(init_test)
+
+## Compare the distributions - test for each variable - should have a similar distrib:
+mice::densityplot(init_test, ~ LA)
+mice::densityplot(init_test, ~ SLA)
+mice::densityplot(init_test, ~ SeedMass)
+mice::densityplot(init_test, ~ StemSpecDens)
+# prefer for the imputed data to be plausible values, i.e. values that could
+# ... have been observed if they had not been missing.
+mice::stripplot(init_test, LA ~ .imp, pch = 20, cex = 2)
+mice::stripplot(init_test, SLA ~ .imp, pch = 20, cex = 2)
+mice::stripplot(init_test, SeedMass ~ .imp, pch = 20, cex = 2)
+mice::stripplot(init_test, StemSpecDens ~ .imp, pch = 20, cex = 2)
+mice::stripplot(init_test)
+
+# Get the imputed values for each iteration?
+# If the objective of the imputation is to produce estimates of missing
+# values (for example, to fill gaps in a dataset), single imputation is
+# considered most effective, because the stochastic draws in multiple
+# imputation add error (Van Buuren, 2012)
+iter_data <- mice::complete(init_test, action = "long")
+
+
+
+# Save imputed traits:
+saveRDS(complete_data, here::here("transformed_data",
+                                         "final_traits_TREES.rds"))
+
+
+
+# 3 - Test if ok to impute traits - cross validation ===========================
+
+
+cross_val_results <- test.mf(raw_sp_tr = sp_tr_TREES)
+
+
+
+
