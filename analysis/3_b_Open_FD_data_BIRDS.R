14d6366c560060c2a3796bfec35f7c743cb4998b
CmlMagneville <camille.magneville@gmail.com>
2024-04-18 13:33:02 +0200

    :green_heart: Give new nms to birds tr

--- analysis/3_b_Open_FD_data_BIRDS.R
+++ analysis/3_b_Open_FD_data_BIRDS.R
@@ -75,6 +75,36 @@ birds_traits_df <- birds_traits_df %>%
   dplyr::select(-c("Beak.Length_Culmen", "Beak.Width"))
 
 
+# Give new names to the traits:
+birds_traits_df <- birds_traits_df %>%
+  dplyr::rename(Age_First_Breeding = "Age of first breeding") %>%
+  dplyr::rename(Hand_Wing_Index = "Hand-Wing.Index") %>%
+  dplyr::rename(Tail_Length = "Tail.Length") %>%
+  dplyr::rename(Tarsus_Length = "Tarsus.Length") %>%
+  dplyr::rename(Clutch_Mean = "Clutch_MEAN") %>%
+  dplyr::rename(Broods_Per_Year = "Broods per year") %>%
+  dplyr::rename(Life_Span = "Life span") %>%
+  dplyr::rename(Fledging_Period = "Fledging period") %>%
+  dplyr::rename(Beak_Ratio = "Beak.Ratio")
+
+
+# Format the traits:
+birds_traits_df$Age_First_Breeding <- ordered(birds_traits_df$Age_First_Breeding,
+                                               levels = c("1", "2", "3", "4", "5", "6"))
+birds_traits_df$Mass <- as.numeric(birds_traits_df$Mass)
+birds_traits_df$Hand_Wing_Index <- as.numeric(birds_traits_df$Hand_Wing_Index)
+birds_traits_df$Migration <- ordered(birds_traits_df$Migration,
+                                 levels = c("1", "2", "3"))
+birds_traits_df$Tail_Length <- as.numeric(birds_traits_df$Tail_Length)
+birds_traits_df$Tarsus_Length <- as.numeric(birds_traits_df$Tarsus_Length)
+birds_traits_df$Clutch_Mean <- as.numeric(birds_traits_df$Clutch_Mean)
+birds_traits_df$Broods_Per_Year <- ordered(birds_traits_df$Broods_Per_Year,
+                                         levels = c("1", "1.5", "2", "2.5",
+                                                    "3", "4.5"))
+birds_traits_df$Life_Span <- as.numeric(birds_traits_df$Life_Span)
+birds_traits_df$Fledging_Period <- as.numeric(birds_traits_df$Fledging_Period)
+birds_traits_df$Beak_Ratio <- as.numeric(birds_traits_df$Beak_Ratio)
+
 
 # Save the traits:
 saveRDS(birds_traits_df, file = here::here("transformed_data",
