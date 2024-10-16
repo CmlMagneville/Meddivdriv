f85d4eb26789d2141e539b592ea31012a34c928b
CmlMagneville <camille.magneville@gmail.com>
2024-10-16 10:55:41 +0200

    :green_heart: Rf FRic REPTILES

--- analysis/8_d_Random_forests_all_taxa_FD_FRic.R
+++ analysis/8_d_Random_forests_all_taxa_FD_FRic.R
@@ -254,9 +254,9 @@ varimp_reptiles <- test.rf.model(rf_data = rf_fric_reptiles_df,
 varimp_reptiles[[1]]
 # Std Variable importance:
 varimp_reptiles[[2]]
-# Mean R-squared: 0.4656991
+# Mean R-squared: 0.347773
 varimp_reptiles[[3]]
-# Sd R-squared: 0.009807748
+# Sd R-squared: 0.01106378
 varimp_reptiles[[4]]
 
 # Save variable importance:
