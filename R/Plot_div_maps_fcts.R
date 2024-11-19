6021d2eb0c78044894f78232918c6f7f6b3c9726
CmlMagneville <camille.magneville@gmail.com>
2024-11-19 15:58:17 +0100

    :art: Maps of main drivers

--- R/Plot_div_maps_fcts.R
+++ R/Plot_div_maps_fcts.R
@@ -179,84 +179,104 @@ div.maps.plot <- function(div_per_cell_df,
 
 
 
-div.3d.plots <- function(div_per_cell_list,
-                         div_facet_nm,
-                         taxa_nm,
-                         grid,
-                         plot_title,
-                         save) {
+#' Maps drivers
+#'
+#' @param drivers_df the data frame with drivers values
+#' @param driver_nm a character string referring to the driver to study
+#' (named as in the associated column in \code{drivers_df})
+#' @param grid a \code{sf} object referring to the spatial grid used, either
+#' the 10x10km grid (WOODIV one) or the 50x50km one (INTEGRADIV one).
+#' @param col_pal a vector gathering two colors for the gradient from low to high
+#' @param land_mask a shapefile of europe land mask
+#' @param save TRUE or FALSE whether the plot is to be saved or not
+#'
+#' @return
+#' @export
+#'
 
-  print("Make sure that the list is ordered with 1 - richness, 2 - dispersion,
-        3 - originality dataframes")
-
-  # Rename the column with metrics name:
-  for (i in c(1:length(div_per_cell_list))) {
-
-    dim_nm <- names(div_per_cell_list)[i]
-
-    colnames(div_per_cell_list[[i]])[2] <- dim_nm
-
-  }
-
-  # Link the three tables:
-  for (i in  c(1:length(div_per_cell_list))){
-
-    if (i == 1){
-      dim_div_df <- div_per_cell_list[[i]][, c(1:2)]
-    }
-    if (i != 1){
-      dim_div_df <- dplyr::full_join(dim_div_df,
-                                     div_per_cell_list[[i]][, c(1:2)])
-    }
-  }
-
-  # rename the columns:
-  colnames(dim_div_df)[c(2:4)] <- c("richness",
-                                    "dispersion",
-                                    "originality")
+drivers.maps.plot <- function(drivers_df,
+                              driver_nm,
+                              grid,
+                              col_pal,
+                              land_mask,
+                              save) {
 
 
-  # Compute the values for plotting:
-  col_code <- colors3d::colors3d(data = dim_div_df[, -1])
-  dim_div_df$col_code <- col_code
 
-  # Link to thegrid so have spatial info:
-  spatial_div_per_cell_df <- dplyr::left_join(grid, dim_div_df,
+  # Make sure that same format for the Idgrid column in div and grid objects:
+  drivers_df <- dplyr::mutate(drivers_df,
+                              Idgrid = as.character(Idgrid))
+  grid <- dplyr::mutate(grid, Idgrid = as.character(Idgrid))
+
+  # Link the div_per_cell_df and the grid so can have spatial info:
+  spatial_driv_df <- dplyr::left_join(grid, drivers_df,
                                               by = "Idgrid")
 
-  #
-  rgl::plot3d(spatial_div_per_cell_df$richness,
-              spatial_div_per_cell_df$dispersion,
-              spatial_div_per_cell_df$originality,
-              col = spatial_div_per_cell_df$col_code,
-              xlab = "Richness" ,
-              ylab = "Dispersion",
-              zlab = "Originality")
+  # Some rows have Div_metric = NA, remove them:
+  spatial_driv_df <- dplyr::filter(spatial_driv_df,
+                                  ! is.na(get(driver_nm)))
 
+  # Create a box around the study area:
+  box_m <- sf::st_bbox(grid)
 
+  # Rename the driver so can take the given column:
+  spatial_driv_df <- spatial_driv_df %>%
+    dplyr::rename(driver_plot = driver_nm)
+
+  # Plot the map:
   plot_map <- ggplot2::ggplot() +
-    ggplot2::geom_sf(data = land_mask, fill = "lightgrey", color = "#636363") +
-    ggplot2::geom_sf(data= spatial_div_per_cell_df, ggplot2::aes(fill = col_code),
-                     color = "lightgrey") +
-    ggplot2::scale_fill_distiller(type = "div", limit = limit) +
-    ggplot2::theme(legend.position = "right")  +
-    ggplot2::theme(legend.title = ggplot2::element_text(size = 12,
+      ggplot2::geom_sf(data = land_mask, fill = "lightgrey", color = "#636363") +
+      ggplot2::geom_sf(data = spatial_driv_df, ggplot2::aes(fill = driver_plot),
+                       color = "lightgrey") +
+      ggplot2::scale_fill_gradient(low = col_pal[1], high = col_pal[2]) +
+      ggplot2::theme(legend.position = "right")  +
+      ggplot2::theme(legend.title = ggplot2::element_text(size = 12,
+                                                          face = "bold")) +
+      ggplot2::theme(axis.text = ggplot2::element_text(size = 10),
+                     axis.title = ggplot2::element_text(size = 12,
                                                         face = "bold")) +
-    ggplot2::theme(axis.text = ggplot2::element_text(size = 10),
-                   axis.title = ggplot2::element_text(size = 12,
-                                                      face = "bold")) +
-    ggplot2::labs(x = "Longitude", y = "Latitude",
-                  fill = metric_nm) +
-    ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "lightgrey",
-                                                            linewidth = 0.1)) +
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white",
-                                                            colour = "darkgrey"))+
-    ggplot2::coord_sf(xlim = c(box_m["xmin"], box_m["xmax"]),
-                      ylim = c(box_m["ymin"], box_m["ymax"]))
+      ggplot2::labs(x = "Longitude", y = "Latitude",
+                    fill = "") +
+      ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "lightgrey",
+                                                              linewidth = 0.1)) +
+      ggplot2::theme(panel.background = ggplot2::element_rect(fill = "white",
+                                                              colour = "darkgrey"))+
+      ggplot2::coord_sf(xlim = c(box_m["xmin"], box_m["xmax"]),
+                        ylim = c(box_m["ymin"], box_m["ymax"])) +
+      ggplot2::ggtitle(paste0("Distribution of", sep = " ", driver_nm))
 
 
+  # Print the plot:
+  print(plot_map)
 
+  # Save if needed:
+  if (save == TRUE) {
 
+    ggplot2::ggsave(plot = plot_map,
+                    filename = here::here("outputs",
+                                          "drivers_maps",
+                                          paste0(driver_nm,
+                                                 sep = ".",
+                                                 "pdf")),
+                    device = "pdf",
+                    scale = 1,
+                    height = 3000,
+                    width = 5000,
+                    units = "px",
+                    dpi = 600)
+    ggplot2::ggsave(plot = plot_map,
+                    filename = here::here("outputs",
+                                          "drivers_maps",
+                                          paste0(driver_nm,
+                                                 sep = ".",
+                                                 "jpg")),
+                    device = "jpg",
+                    scale = 1,
+                    height = 3000,
+                    width = 5000,
+                    units = "px",
+                    dpi = 600)
 
+  }
 
 }
